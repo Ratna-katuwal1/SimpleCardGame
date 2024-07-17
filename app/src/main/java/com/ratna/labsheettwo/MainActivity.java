3a3115802433c1fbc006bb40db5f2f0aca061ca5
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-07-17 22:24:47 +0545

    top design

--- app/src/main/java/com/ratna/labsheettwo/MainActivity.java
+++ app/src/main/java/com/ratna/labsheettwo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ratna.labsheettwo;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,56 +13,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    ImageView blackCard, redCard;
-    Button reset, play;
-    TextView msgTextView;
+    TextView redCardValue, lifeValue, blackCardValue, resultTextView;
+    ImageView redCardImage, blackCardImage;
+    Button playButton, resetButton;
+    int redCardScore = 0;
+    int blackCardScore = 0;
+    int life = 3;
+    Random random;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        redCardValue = findViewById(R.id.redCardValue);
+        lifeValue = findViewById(R.id.lifeValue);
+        blackCardValue = findViewById(R.id.blackCardValue);
+        resultTextView = findViewById(R.id.resultTextView);
+        redCardImage = findViewById(R.id.redCardImage);
+        blackCardImage = findViewById(R.id.blackCardImage);
+        playButton = findViewById(R.id.playButton);
+        resetButton = findViewById(R.id.resetButton);
+        random = new Random();
+        handler = new Handler();
 
-        blackCard = findViewById(R.id.blackCard);
-        redCard = findViewById(R.id.redCard);
-        reset = findViewById(R.id.reset);
-        play = findViewById(R.id.btn);
-        msgTextView = findViewById(R.id.draw);
+        playButton.setOnClickListener(view -> {
 
-        play.setOnClickListener(view -> {
-            int blackNum = randomNumber();
-            int redNum = randomNumber();
-            int resource1 = getResources().getIdentifier("black" + blackNum, "drawable", getPackageName());
-            blackCard.setImageResource(resource1);
-            int resource2 = getResources().getIdentifier("red" + redNum, "drawable", getPackageName());
-            redCard.setImageResource(resource2);
-            checkWin(blackNum, redNum);
         });
 
-        reset.setOnClickListener(view -> reset());
+        resetButton.setOnClickListener(view -> {
+
+        });
     }
 
-    private int randomNumber() {
-        Random randomNum = new Random();
-        return randomNum.nextInt(13);
-    }
+    private void playGame(){
+        int redCardValue = random.nextInt(13) + 1;
+        int blackCardValue = random.nextInt(13) + 1;
 
-    private void checkWin(int blackNum, int redNum) {
-        String result;
-        if (blackNum > redNum) {
-            result = "Black Wins!";
-        } else if (blackNum < redNum) {
-            result = "Red Wins!";
-        } else {
-            result = "Draw!";
-        }
-        msgTextView.setText(result);
-    }
-
-    private void reset() {
-        blackCard.setImageResource(R.drawable.black0);
-        redCard.setImageResource(R.drawable.red0);
-        msgTextView.setText("");
     }
 }
