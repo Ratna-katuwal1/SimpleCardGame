1e4ee5d7ba36d78f7bd94221204b26fb4dc38969
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-07-18 15:43:45 +0545

    top design

--- app/src/main/java/com/ratna/labsheettwo/MainActivity.java
+++ app/src/main/java/com/ratna/labsheettwo/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     int redCardScore = 0;
     int blackCardScore = 0;
     int life = 3;
+    int drawCount = 0;
     Random random;
     Handler handler;
 
@@ -39,17 +40,71 @@ public class MainActivity extends AppCompatActivity {
         handler = new Handler();
 
         playButton.setOnClickListener(view -> {
-
+            playGame();
         });
 
         resetButton.setOnClickListener(view -> {
-
+            resetGame();
         });
     }
 
-    private void playGame(){
-        int redCardValue = random.nextInt(13) + 1;
-        int blackCardValue = random.nextInt(13) + 1;
+    private void playGame() {
+        int redCard = random.nextInt(13) + 1;
+        int blackCard = random.nextInt(13) + 1;
 
+        redCardImage.setImageResource(getResources().getIdentifier("red" + redCard, "drawable", getPackageName()));
+        blackCardImage.setImageResource(getResources().getIdentifier("black" + blackCard, "drawable", getPackageName()));
+
+        if (redCard > blackCard) {
+            redCardScore++;
+            redCardValue.setText(String.valueOf(redCardScore));
+        } else if (blackCard > redCard) {
+            blackCardScore++;
+            blackCardValue.setText(String.valueOf(blackCardScore));
+        } else {
+            drawCount++;
+            life--;
+            lifeValue.setText(String.valueOf(life));
+            if (life <= 0) {
+                displayResult();
+                return;
+            }
+        }
+        lifeValue.setText(String.valueOf(life));
+        if (life <= 0) {
+            displayResult();
+        }
+    }
+
+    private void displayResult() {
+        String result;
+        if (redCardScore > blackCardScore) {
+            result = "Red Card Won the game by " + redCardScore + "points";
+        } else if (blackCardScore > redCardScore) {
+            result = "Black Card Won the game by " + blackCardScore + "points";
+        } else {
+            result = "the game is draw";
+        }
+        resultTextView.setText(result);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                resetGame();
+            }
+        }, 5000);
+    }
+
+    private void resetGame() {
+        redCardScore = 0;
+        blackCardScore = 0;
+        life = 3;
+        drawCount = 0;
+        redCardValue.setText(String.valueOf(redCardScore));
+        blackCardValue.setText(String.valueOf(blackCardScore));
+        lifeValue.setText(String.valueOf(life));
+        resultTextView.setText("");
+        redCardImage.setImageResource(R.drawable.red0);
+        blackCardImage.setImageResource(R.drawable.black0);
     }
 }
